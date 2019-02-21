66668c675376deab38423d2967e3321aaf6b479b
Benny Powers <benny.powers@forter.com>
2019-02-21 11:22:10 +0200

    Fix bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function loader(source) {
   return `
     import { css } from 'lit-element';
-    export default css\`${ JSON.stringify(source) }\`;
+    export default css\`${ source }\`;
   `;
 }
 
