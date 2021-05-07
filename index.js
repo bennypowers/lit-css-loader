6f8b0496dd5db8d5efb42910f8ccc32f4c2fbf22
Serhii Kulykov <iamkulykov@gmail.com>
2021-05-07 14:53:45 +0300

    Prevent warning when using with Lit 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function loader(source) {
   return `
-    import { css } from 'lit-element';
+    import { css } from 'lit-element/lit-element.js';
     export default css\`${ source.replace(/(`|\\|\${)/g, '\\$1') }\`;
   `;
 }
