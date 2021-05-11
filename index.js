7004d40ed1357555c71a753d717bd09030f8a1cb
web-padawan <iamkulykov@gmail.com>
2021-05-11 10:56:48 +0300

    feat!: upgrade to use Lit 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function loader(source) {
   return `
-    import { css } from 'lit-element';
+    import { css } from 'lit';
     export default css\`${ source.replace(/(`|\\|\${)/g, '\\$1') }\`;
   `;
 }
